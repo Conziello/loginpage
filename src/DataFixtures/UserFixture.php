84aa0029e6d81e4db3a22f8994301075dabf87e6
conziello <conziello@gmail.com>
2020-11-10 16:13:39 +0300

    changes on user fixture

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -2,16 +2,30 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class UserFixture extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user -> setUsername("admin");
+        $user-> setPassword(
+            $this ->encoder->encodePassword($user, '0000')
+        );
+        $user-> setEmail('aaaa@gmail.com');
 
+
+        $manager->persist($user);
         $manager->flush();
     }
 }
